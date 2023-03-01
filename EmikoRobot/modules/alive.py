e880b26041d27477f178df0f01250dd059b8d129
Rafli Zaki <124425133+raflizaki@users.noreply.github.com>
2023-03-01 09:52:25 +0700

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -14,11 +14,11 @@ PHOTO = "https://telegra.ph/file/fe73013dd30cbec4d9f50.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm 𝚁𝙰𝙼𝙱𝙴𝙻 𝚁𝙾𝙱𝙾𝚃.** \n\n"
-  TEXT += "✘ **I'm Working Properly** \n\n"
-  TEXT += f"✘ **My Master : [MANDOR](https://t.me/quntulharam)** \n\n"
-  TEXT += f"✘ **Library Version :** `{telever}` \n\n"
-  TEXT += f"✘ **Telethon Version :** `{tlhver}` \n\n"
-  TEXT += f"✘ **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += "🇮🇩 **I'm Working Properly** \n\n"
+  TEXT += f"🇮🇩 **My Master : [MANDOR](https://t.me/quntulharam)** \n\n"
+  TEXT += f"🇮🇩 **Library Version :** `{telever}` \n\n"
+  TEXT += f"🇮🇩 **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"🇮🇩 **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**🇮🇩Thanks For Adding Me Here 🇮🇩**"
   BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/Trymaskosbot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/pantekyks")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
